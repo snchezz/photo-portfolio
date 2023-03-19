68009fda74e8f9aa04d630730de82840ec9b4a72
Andres <sitosanzdev@gmail.com>
2023-03-20 00:07:17 +0100

    Photo-portfolio

---
+++ js/init.js
@@ -0,0 +1,13 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+$(document).ready(function(){
+  $('.materialboxed').materialbox();
+});
+      
